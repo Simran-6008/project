81f5b6437fb8ab550358bf35865fab4bb5bef690
Simran <simrantamang6008@gmail.com>
2024-01-03 15:43:58 +0530

    Create timeline project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import Time from "./Time";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Welcome to Simran's Timeline</h1>
+      <br />
+      <Time year={2002} event="I was born." />
+      <Time year={2005} event="I went to school first time." />
+      <Time
+        year={2008}
+        event="A sheep hit me with its horn and he pulled me into water."
+      />
+      <Time year={2014} event="I had my periods for first time." />
+      <Time
+        year={2017}
+        event="Along with my friends group got beaten by sir in fron of everyone in school. It was a very experience for me."
+      />
+      <Time year={2017} event="I joined sports." />
+      <Time
+        year={2019}
+        event="I won silver medal for kangra team in athletics meet at Hamirpur."
+      />
+      <Time
+        year={2020}
+        event="This year is very difficult for me beacuse I had to choose college or either sports university. But beacuse of my parents I had to go to college instead of sports university."
+      />
+      <Time year={2023} event="I join Sjahe Sapne" />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Time.jsx
@@ -0,0 +1,12 @@
+const Time = ({year,event}) => {
+    return (
+      <>
+        <h2>
+          {year}
+        </h2>
+        <p> {event}</p>
+      </>
+    );
+  };
+  export default Time;
+  
