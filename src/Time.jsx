136a1f7fa2300f9efc3cd9d83736c9d59d232a1c
Simran <simrantamang6008@gmail.com>
2024-01-05 11:27:26 +0530

    Use external css for project

--- src/Time.jsx
+++ src/Time.jsx
@@ -1,12 +1,10 @@
-const Time = ({year,event}) => {
-    return (
-      <>
-        <h2>
-          {year}
-        </h2>
-        <p> {event}</p>
-      </>
-    );
-  };
-  export default Time;
-  
+import "./Time.css";
+const Time = ({ year, event }) => {
+  return (
+    <>
+      <h2 className="heading">{year}</h2>
+      <p> {event}</p>
+    </>
+  );
+};
+export default Time;
